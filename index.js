b90112dffb160230b13e2df2b4758a54c36863e1
Mohammad Apon <umar426.net@gmail.com>
2023-05-02 16:49:07 +0600

    data.json added

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const cors=require('cors')
 const port = 5000;
+const data = require('./data.json')
 
 app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('from server')
+})
+
+app.get('/data',(req,res)=>{
+    res.send(data)
 })
 
 
